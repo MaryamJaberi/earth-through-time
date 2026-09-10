e35d5954f39993fca42f4ac01c8b60ab2cd74aed
Maryam Jaberi <48519581+MaryamJaberi@users.noreply.github.com>
2026-09-10 14:20:18 +0200

    More era photos: fossils, cities, future

---
+++ media2.js
@@ -0,0 +1,18 @@
+(function(){var U=function(f){return "https://commons.wikimedia.org/wiki/Special:FilePath/"+encodeURIComponent(f)+"?width=1280"};
+function set(id,title,year,epoch,imgs){var E=(window.EARTH_ERAS||[]).find(function(x){return x.id===id});if(!E)return;E.titleFa=title;E.yearLabelFa=year;E.epochFa=epoch;E.images=imgs.map(function(r){return {src:U(r[0]),cap:r[1],capFa:r[2]};});}
+set("columbia","کلمبیا و یاخته‌های پیچیده","۱٫۸ میلیارد سال پیش","نونا / کلمبیا",[["Grypania.jpg","Grypania","گریپانیا"],["Chert.jpg","Chert","چرت"]]);
+set("rodinia","رودینیا","۱٫۰ میلیارد سال پیش","رودینیا",[["Rodinia_reconstruction.jpg","Rodinia","بازسازی رودینیا"],["Grand_Canyon_from_Lipan_Point.jpg","Grand Canyon","گرند کنیون"]]);
+set("ediacaran","پس از گلوله برفی","۵۷۵ میلیون سال پیش","ادیاکاران",[["DickinsoniaCostata.png","Dickinsonia","دیکینسونیا"],["Spriggina_floundensi.jpg","Spriggina","اسپریگینا"],["Charnia_masoni.jpg","Charnia","چارنیا"]]);
+set("devonian","جنگل و ماهی","۳۹۰ میلیون سال پیش","دونین",[["Tiktaalik_BW.jpg","Tiktaalik","تیکتالیک"],["Dunkleosteus_intermedius.jpg","Dunkleosteus","دانکلیوستئوس"],["Archaeopteris.jpg","Archaeopteris","آرکئوپتریس"]]);
+set("carboniferous","باتلاق زغال","۳۱۰ میلیون سال پیش","کربونیفر",[["Meganeura_monyi.jpg","Meganeura","مگانورا"],["Lepidodendron.jpg","Lepidodendron","لپیدودندرون"],["Coal.jpg","Coal","زغال"]]);
+set("permian","پانگه‌آ کامل","۲۶۰ میلیون سال پیش","پرمین",[["Pangaea_continents.png","Pangaea","پانگه‌آ"],["Dimetrodon_grandis.jpg","Dimetrodon","دیمترودون"]]);
+set("end-permian","مرگ بزرگ","۲۵۲ میلیون سال پیش","مرز پرمین–تریاس",[["Siberian_Traps_map.png","Siberian Traps","تله‌های سیبری"],["Lystrosaurus.jpg","Lystrosaurus","لیستروسور"]]);
+set("kpg","زمستان برخورد","۶۶ میلیون سال پیش","مرز ک–پ",[["Chicxulub_radar_topography.jpg","Chicxulub","چیکشولوب"],["KT_boundary_in_Colorado.jpg","K-Pg clay","رس مرز ک–پ"]]);
+set("eocene","گلخانه‌ای ائوسن","۵۰ میلیون سال پیش","ائوسن",[["Basilosaurus_cetoides.jpg","Basilosaurus","بازیلوسور"],["Himalayas.jpg","Himalaya","هیمالیا"]]);
+set("holocene","کشت و روستا","۸ هزار سال پیش","هولوسن",[["Catalhoyuk_mural.jpg","Catalhoyuk","چاتال‌هویوک"],["Emmer_wheat.jpg","Emmer wheat","گندم امر"]]);
+set("antiquity","جهان کلاسیک","۳۰۰ پیش از میلاد","عصر کلاسیک",[["Persepolis.jpg","Persepolis","تخت جمشید"],["Library_of_Celsus.jpg","Celsus library","کتابخانهٔ سلسوس"],["Rosetta_Stone.jpg","Rosetta Stone","سنگ روزتا"]]);
+set("scientific-revolution","انقلاب علمی","۱۶۸۰ میلادی","انقلاب علمی",[["Leeuwenhoek_microscope.png","Microscope","میکروسکوپ لیوونهوک"],["Hooke-flea.jpg","Hooke flea","کک هوک"]]);
+set("darwin","فرگشت علم می‌شود","۱۸۵۹ میلادی","سدهٔ نوزدهم",[["Charles_Darwin_by_Julia_Margaret_Cameron_2.jpg","Darwin","داروین"],["HMS_Beagle_by_Conrad_Martens.jpg","Beagle","بیگل"]]);
+set("plus100","یک سده بعد","۲۱۲۶ میلادی","سناریو",[["Sea_level_rise.jpg","Sea level", "سیل ساحلی"],["Greenland_ice_sheet.jpg","Greenland ice","یخ گرینلند"]]);
+set("plus1000","هزار سال بعد","۳۰۲۶ میلادی","سناریوی گمانی",[["The_Earth_seen_from_Apollo_17.jpg","Earth / NASA","همان سیاره — ناسا"],["Blue_Marble_2002.png","Blue Marble","مرمر آبی"]]);
+})();
